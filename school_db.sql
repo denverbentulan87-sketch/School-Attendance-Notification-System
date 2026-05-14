c4e5df6e7ab93b2ed706baabbb67297e75655c32
denverbentulan87-sketch <denverbentulan87@gmail.com>
2026-05-14 10:54:53 +0800

    last full commit(export final db)

--- school_db.sql
+++ school_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2026 at 11:29 AM
+-- Generation Time: May 14, 2026 at 04:54 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,7 +32,7 @@ CREATE TABLE `attendance` (
   `student_id` int(11) NOT NULL,
   `scan_date` date NOT NULL,
   `scan_time` time NOT NULL,
-  `status` enum('on_time','late') DEFAULT 'on_time',
+  `status` enum('on_time','present','late','absent') NOT NULL DEFAULT 'on_time',
   `date_added` datetime DEFAULT current_timestamp(),
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -42,9 +42,12 @@ CREATE TABLE `attendance` (
 --
 
 INSERT INTO `attendance` (`id`, `student_id`, `scan_date`, `scan_time`, `status`, `date_added`, `created_at`) VALUES
-(5, 42, '2026-05-02', '14:43:10', 'late', '2026-05-02 14:43:10', '2026-05-02 14:43:10'),
-(6, 40, '2026-05-02', '14:44:09', 'late', '2026-05-02 14:44:09', '2026-05-02 14:44:09'),
-(7, 43, '2026-05-02', '16:03:57', '', '2026-05-02 16:03:57', '2026-05-02 16:03:57');
+(20, 56, '2026-05-10', '10:36:43', 'late', '2026-05-10 10:36:43', '2026-05-10 10:36:43'),
+(33, 56, '2026-05-13', '17:00:00', 'absent', '2026-05-13 22:29:15', '2026-05-13 22:29:15'),
+(34, 81, '2026-05-13', '17:00:00', 'absent', '2026-05-13 22:29:19', '2026-05-13 22:29:19'),
+(35, 81, '2026-05-14', '08:00:38', 'present', '2026-05-14 08:00:38', '2026-05-14 08:00:38'),
+(36, 56, '2026-05-14', '09:04:21', 'late', '2026-05-14 09:04:21', '2026-05-14 09:04:21'),
+(37, 82, '2026-05-14', '10:31:01', 'late', '2026-05-14 10:31:01', '2026-05-14 10:31:01');
 
 -- --------------------------------------------------------
 
@@ -61,6 +64,18 @@ CREATE TABLE `notifications` (
   `is_read` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `student_id`, `message`, `created_at`, `sender`, `is_read`) VALUES
+(18, 56, 'Your child Kimmy Arda was marked LATE at 10:36 AM.', '2026-05-10 10:36:43', 'system', 1),
+(31, 56, 'Your child Kimmy Arda was marked ABSENT on May 13, 2026. No QR scan was recorded.', '2026-05-13 22:29:19', 'system', 0),
+(32, 81, 'Your child Denver Bentulan was marked ABSENT on May 13, 2026. No QR scan was recorded.', '2026-05-13 22:29:23', 'system', 0),
+(33, 81, 'Your child Denver Bentulan was marked PRESENT at 08:00 AM.', '2026-05-14 08:00:38', 'system', 0),
+(34, 56, 'Your child Kimmy Arda was marked LATE at 09:04 AM.', '2026-05-14 09:04:21', 'system', 0),
+(35, 82, 'Your child Russel Lupian was marked LATE at 10:31 AM.', '2026-05-14 10:31:01', 'system', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -75,18 +90,6 @@ CREATE TABLE `user` (
   `role` enum('student','admin','parent') NOT NULL,
   `parent_email` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `name`, `email`, `password`, `role`, `parent_email`) VALUES
-(3, 'Russel Lupian', 'russellupian37@gmail.com', '$2y$10$4UAoRWnvkLO0lWUvENlS6eWPan8NsYIqygZdh0vyoXy2v/srhl/be', 'student', NULL),
-(4, 'Denver Bentulan', 'denverbentulan87@gmail.com', '$2y$10$ME5S6fxzSr1y3tMAA65Cru3JCOQEI9fA8uIxDQ9JspugbqdF1eKiG', 'student', NULL),
-(5, 'Van Kim Arda', 'ardavankim69@gmail.com', '$2y$10$QJaxNT1FAR9jBOATASpqYOMU1w8/xf3RSBWn7N/vySKpBfSSnqca.', 'student', NULL),
-(6, 'Clark Nam-Ay', 'clarkkyy27@gmail.com', '$2y$10$5MfyKV1i2ySEkAI0i9yqg.JjWxI0UPTs2aO9.NM0TJsBPGaNwYP4u', 'student', NULL),
-(7, 'Lito Lapid', 'litolapid@gmail.com', '$2y$10$8oXaTHXCNzCa5mr1wnnmF.imC/OZFD/AATspxodkH7fsrTssY.oke', 'student', NULL),
-(8, 'Juan Dela Cruz', 'juandelacruz@gmail.com', '$2y$10$ovYYGaEU7vkc9fRqIh.mm.uAP0IojpV6PFMc1xGuVabk70k6YvV/y', 'student', NULL);
 
 -- --------------------------------------------------------
 
@@ -111,11 +114,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `fullname`, `email`, `role`, `password`, `date_added`, `parent_email`, `qr_code`, `qr_token`) VALUES
-(1, 'Denver Bentulan', 'denverbentulan87@gmail.com', 'admin', '$2y$10$YG.eXrTA4TZlH3jEmt73Xe0jrCFP2hasnjPlL3fMvppm4Epb.HDla', '2026-04-13 18:20:27', NULL, NULL, NULL),
+(1, 'Denver Bentulan', 'denverbentulan87@gmail.com', 'admin', '$2y$10$seOtld2KXpKZaZImlNVHg.dFYzc4udXRqbv8bv.y.b1ys711a.a7y', '2026-04-13 18:20:27', NULL, NULL, NULL),
 (35, 'John Denver Bentulan', 'johndenverbentulan@gmail.com', 'parent', '$2y$10$306wnr4OUr3w4oGqhROuOu2zBEaVRlWL5zKRORkBc4xKpSPCfykSa', '2026-05-02 11:52:57', '', NULL, NULL),
-(40, 'Nicole Torifiel', 'torrefielnicole90@gmail.com', 'student', '$2y$10$YSOfLIrfldxhcEti4Ew7.Oq86i0lbVVMiZiI8sBKSwfazGpBvfV5q', '2026-05-02 13:12:23', 'johndenverbentulan@gmail.com', 'qrcodes/qr_d50f6ee5110d202b4c86f36fc2a990ee.png', 'd50f6ee5110d202b4c86f36fc2a990ee'),
-(42, 'Alvin Mata', 'nicoletorrefiel909@gmail.com', 'student', '$2y$10$YBDfDYTzG7kxh2th0a4b0OailS5HBpNaR2t2MXukLuXO730NJjqcG', '2026-05-02 14:04:20', 'torrefielnicole90@gmail.com', 'qrcodes/qr_efb2ca8765b6d701bc222e727fdde842.png', 'efb2ca8765b6d701bc222e727fdde842'),
-(43, 'Joe Estose', 'sansicas776@gmail.com', 'student', '$2y$10$0gMSkgjvcEsLM27I73ijde6YE.55Xcv9hLYVsahwevcHvi5qSmQwG', '2026-05-02 16:03:23', 'torrefielnicole90@gmail.com', 'qrcodes/qr_415a6e68624d6ddf928869791db8913b.png', '415a6e68624d6ddf928869791db8913b');
+(56, 'Kimmy Arda', 'torrefielnicole90@gmail.com', 'student', '$2y$10$gqffDkzX9JAsVZ1lyEKZPu16iuOvLAIExprQovAUDvJ1Oh.DAPOpe', '2026-05-07 10:54:53', 'johndenverbentulan@gmail.com', 'qrcodes/qr_7c95029080450b9e5813d5f13a16f225.png', '7c95029080450b9e5813d5f13a16f225'),
+(81, 'Denver Bentulan', 'ardavankim8@gmail.com', 'student', '$2y$10$BTTX.4b3srK2yXCidhYsiOTadexavgFBsBG8aMUFVDUXGrvFRoldW', '2026-05-13 21:54:18', 'johndenverbentulan@gmail.com', 'qrcodes/qr_00cf6490e77fa3b1f281116653b818e3.png', '00cf6490e77fa3b1f281116653b818e3'),
+(82, 'Russel Lupian', 'lupianrussel@gmail.com', 'student', '$2y$10$8kS7UtVjyTEyYwzXfA62mOPh5WfikJ8j.1We1Y4aPDgdvVV.blnAO', '2026-05-14 10:26:26', 'johndenverbentulan@gmail.com', 'qrcodes/qr_4459d4db3ca0881785b0639c005c1d5f.png', '4459d4db3ca0881785b0639c005c1d5f');
 
 --
 -- Indexes for dumped tables
@@ -158,13 +161,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -176,7 +179,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
 --
 -- Constraints for dumped tables
@@ -192,7 +195,7 @@ ALTER TABLE `attendance`
 -- Constraints for table `notifications`
 --
 ALTER TABLE `notifications`
-  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `user` (`id`) ON DELETE SET NULL;
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `users` (`id`) ON DELETE SET NULL;
 
 --
 -- Constraints for table `user`
